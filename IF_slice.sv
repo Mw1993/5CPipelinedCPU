62f8dfe4952193d410ae652574cb0cb34a9a2f2e
Maggie White <mawhite6@wisc.edu>
2015-04-03 16:18:31 -0500

    Implemented basic functionality of instruction fetch

--- IF_slice.sv
+++ IF_slice.sv
@@ -1,1 +1,24 @@
+module IF_slice(clk, rst, PCSrc, PCld, PC_inc, instr);
 
+input clk, rst;
+input PCSrc;
+input [15:0] PCld;
+output [15:0] PC_inc;
+output [15:0] instr;
+
+reg [15:0] PC;
+wire [15:0] tmpPC;
+
+assign tmpPC = PCSrc ? PCld : PC_inc;
+assign PC_inc = PC + 4;
+
+always @(posedge clk, posedge rst) begin
+  if(rst)
+    PC <= 16'h0000;
+  else
+    PC <= tmpPC;
+end
+
+IM instMem (.clk(clk),.addr(PC),.rd_en(!rst),.instr(instr));
+
+endmodule
